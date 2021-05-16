7b3cffb0fab0947ece4ba7a1f9c9b11b2e5c3d46
Padmanabh Wattamwar <wattamwarp874@gmail.com>
2021-05-16 14:50:58 +0530

    unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:appyhigh_assignment_flutter/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_music_app_credixco/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
